83dc0a7e613882b4c04a076de3d236ed1be922ca
darkknightishaan@gmail.com <Dumbledon21!>
2022-12-01 12:17:20 -0600

    asqd

--- src/com/starkodinson/Main.java
+++ src/com/starkodinson/Main.java
@@ -7,20 +7,21 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         int[] data = {3, 7, 3, 4, 32, 5, 64, 34, 2, 12, 5, 6, 23, 43, 62, 56, 54};
-        List<TreeNode<Integer>> heap = new LinkedList<>();
+        List<HeapTreeNode> heap = new LinkedList<>();
 
         for (int i = 0; i < data.length; i++)
         {
-            if (i == 0) { heap.add(new TreeNode<Integer>(data[i])); }
+            if (i == 0) { heap.add(new HeapTreeNode(data[i])); }
             else
             {
-                TreeNode<Integer> p = heap.get((i - 1)/ 2);
-                TreeNode<Integer> c = new TreeNode(data[i], p);
+                HeapTreeNode p = heap.get((i - 1)/ 2);
+                HeapTreeNode c = new HeapTreeNode(data[i], p);
                 heap.add(c);
             }
         }
 
         System.out.println("RANDOM: " + heap);
-
+        HeapTreeNode.buildMaxHeap(heap);
+        System.out.println("MAX: " + heap);
     }
 }
